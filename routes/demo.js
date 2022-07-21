847b4b8ef43244009985e6f313d2d48d15105712
Jonathan Cain <jcainuk@gmail.com>
2022-07-21 16:05:42 +0100

    refactor demo js to use ES6 modern syntax

--- routes/demo.js
+++ routes/demo.js
@@ -4,26 +4,26 @@ const db = require('../data/database');
 
 const router = express.Router();
 
-router.get('/', function (req, res) {
+router.get('/', (req, res) => {
   res.render('welcome');
 });
 
-router.get('/signup', function (req, res) {
+router.get('/signup', (req, res) => {
   res.render('signup');
 });
 
-router.get('/login', function (req, res) {
+router.get('/login', (req, res) => {
   res.render('login');
 });
 
-router.post('/signup', async function (req, res) {});
+router.post('/signup', async (req, res) => {});
 
-router.post('/login', async function (req, res) {});
+router.post('/login', async (req, res) => {});
 
-router.get('/admin', function (req, res) {
+router.get('/admin', (req, res) => {
   res.render('admin');
 });
 
-router.post('/logout', function (req, res) {});
+router.post('/logout', (req, res) => {});
 
 module.exports = router;
