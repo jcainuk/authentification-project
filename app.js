12117cffb91987f9841abdda7c4ab608c9263bed
Jonathan Cain <jcainuk@gmail.com>
2022-07-22 22:01:33 +0100

    configure sessionStore and add it to the middleware to save sessions properly

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const session = require('express-session');
-const mongodbStore = require('connect-mongodb-session')
+const mongodbStore = require('connect-mongodb-session');
 
 const db = require('./data/database');
 const demoRoutes = require('./routes/demo');
@@ -11,7 +11,11 @@ const MongoDBStore = mongodbStore(session);
 
 const app = express();
 
-const sessionStore = new MongoDBStore();
+const sessionStore = new MongoDBStore({
+  uri: 'localhost:27017',
+  databaseName: 'auth-demo',
+  collection: 'sessions',
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +27,7 @@ app.use(session({
   secret: 'super-secret',
   resave: false,
   saveUninitialized: false,
-  store: 
+  store: sessionStore,
 }));
 
 app.use(demoRoutes);
